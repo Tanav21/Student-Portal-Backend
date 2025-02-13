e7829e4ea5e956723aeed98ac825c8d1af1d87b5
tanav <tanav@androwebstech.com>
2025-02-14 00:35:58 +0530

    changes

--- models/User.js
+++ models/User.js
@@ -1,22 +1,22 @@
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    phone: { type: String, default: "" },
-    address: { type: String, default: "" },
-    skills: { type: String, default: "" },
-    image: { type: String },
-    dob: { type: Date },
-    education: [
-        {
-            degree: { type: String, required: true },
-            institution: { type: String, required: true },
-            year: { type: Number }
-        }
-    ],
-    appliedCourses: [{ type: mongoose.Schema.Types.ObjectId, ref: "Course" }] // New Field to Track Applied Courses
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  phone: { type: String, default: "" },
+  address: { type: String, default: "" },
+  skills: { type: String, default: "" },
+  image: { type: String },
+  dob: { type: Date },
+  education: [
+    {
+      degree: { type: String, required: true },
+      institution: { type: String, required: true },
+      year: { type: Number },
+    },
+  ],
+  appliedCourses: [{ type: mongoose.Schema.Types.ObjectId, ref: "Course" }], // New Field to Track Applied Courses
 });
 
-module.exports = mongoose.model("User", UserSchema);
+module.exports = mongoose.model("user", UserSchema);
