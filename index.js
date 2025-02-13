bf517c12ec6ee1f10313a26c8bf10b7376ea8b1b
tanav <tanav@androwebstech.com>
2025-02-14 00:16:38 +0530

    change

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ const courseRoutes = require("./routes/course"); // ✅ Add course routes
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
